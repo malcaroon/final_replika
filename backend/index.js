1a22ed64670576cd209871dfd61c8d3154392b96
Joenel <joenelsevellejo831@gmail.com>
2026-06-04 22:50:43 +0800

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+// import userRoutes from './routes/userRoutes.js';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('TEST MIC');
+});
+
+// app.use('/api', userRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
